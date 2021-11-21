5190c5be6b334ff177c643e04941fbbb0f5c0725
meowpunch <jinhoonbae@gmail.com>
2021-11-21 22:19:45 +0100

    rdd + dataset

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Apache Spark with Scala
-- assignments of [an udemy course](https://www.udemy.com/course/apache-spark-with-scala-hands-on-with-big-data/)
+- hands on exercises of [an udemy course](https://www.udemy.com/course/apache-spark-with-scala-hands-on-with-big-data/)
     - [spark core, Resilient Distributed Dataset](https://spark.apache.org/docs/latest/rdd-programming-guide.html#rdd-persistence)
     - [spark SQL, Datasets and Dataframes](https://spark.apache.org/docs/latest/sql-programming-guide.html)
     
 ## Data
 base path: data/
-- social network - fakefriends.csv
-  - columns: id, name, age, # of friends
-- e-commerce - customer-orders.csv
-  - columns: customer_id, product_id, price_id 
+- social network
+  - fakefriends.csv: id, name, age, # of friends
+- e-commerce 
+  - customer-orders.csv: customerId, productId, priceId 
+- superhero
+  - Marvel-names.txt: heroId, heroName
+  - Marvel-graph.txt: heroId, connectionId...
---
+++ src/main/scala/mix/MostObscureSuperheroDataset.scala
@@ -0,0 +1,64 @@
+package mix
+
+import org.apache.log4j._
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+
+/** Find the superhero with the most co-appearances. */
+object MostObscureSuperheroDataset {
+
+  // Function to extract the hero ID and number of connections from each line
+  def countCoOccurrences(line: String): (Int, Int) = {
+    val elements = line.split("\\s+")
+    (elements(0).toInt, elements.length - 1)
+  }
+
+  case class SuperHero(id: Int, name: String)
+
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkSession using every core of the local machine
+    val spark = SparkSession
+      .builder
+      .appName("MostPopularSuperhero")
+      .master("local[*]")
+      .getOrCreate()
+
+    val superHeroSchema = new StructType()
+      .add("id", IntegerType, nullable = false)
+      .add("name", StringType, nullable = false)
+
+    import spark.implicits._
+    val names = spark.read
+      .schema(superHeroSchema)
+      .option("sep", " ")
+      .csv("data/Marvel-names.txt")
+      .as[SuperHero]
+
+    // Load up the superhero co-appearance data
+    val lines = spark.sparkContext.textFile("data/Marvel-graph.txt")
+
+    // Convert to (heroID, number of connections) Dataset and sum by Id
+    val connections = lines.map(countCoOccurrences).toDF("id", "connections")
+      .groupBy("id").agg(sum("connections").alias("connections"))
+
+    // Compute the actual smallest number of connections in the dataset
+    val minConnections = connections.agg(min("connections")).first().getLong(0)
+
+    // List the names of all superheroes with only one connection
+    connections
+      .filter($"connections" === minConnections)
+      .join(names, "id")
+      .select("name")
+      .show()
+
+    spark.stop()
+
+  }
+}
